f9eaf41e8888de0d0f1c504650206ecf556fa2c5
r-katarzyna <katarzyna.monika.rebelska@gmail.com>
2025-10-18 18:44:42 +0200

    First test case

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,8 +1,22 @@
 import { test, expect } from '@playwright/test';
-import { loginCredentials } from './testData';
+import { credentials } from './testData';
+import { mainPageElements, signInPage, accountMenu } from './locators';
+
 
 test.beforeEach('As a user, I want to enter Amazon webpage', async ({ page }) => { 
 
-  await page.goto(loginCredentials.urlMainPage);
+  await page.goto(credentials.urlMainPage);
 
-}); 
+});
+
+test('As a user, I want to sign in.', async ({page}) => {
+
+  await page.getByRole('link', {name: mainPageElements.yourAccountBtn}).hover();
+  await page.getByText(mainPageElements.accountBtn, {exact: true}).click();
+  await page.getByRole('link', {name: accountMenu.signInBtn, exact: true}).click();
+  await page.fill(signInPage.emailInput, credentials.username);
+  await page.click(signInPage.continueBtn);
+  await page.click(signInPage.proceedBtn);
+  await page.fill(signInPage.nameInput, credentials.name);
+
+});
---
+++ tests/locators.ts
@@ -0,0 +1,14 @@
+export const mainPageElements = {
+    yourAccountBtn : "Your Account",
+    accountBtn : "Account & Lists",
+};
+export const accountMenu = {
+    signInBtn : "Sign in",
+};
+
+export const signInPage = {
+    emailInput : 'input[type=email]', 
+    continueBtn : "#continue",
+    proceedBtn : "#intention-submit-button",
+    nameInput : "#ap_customer_name",
+};
